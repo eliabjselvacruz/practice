b5ea51fef70955a81c3b3073c83631e39b33de47
eliabjselvacruz <eliabjselvacruz51@gmail.com>
2022-03-01 16:49:50 -0600

    Functions scope.

---
+++ closures-scope-javascript/src/scope/functions.js
@@ -0,0 +1,18 @@
+const fruits = () => {
+  var fruit = "Apple";
+  console.log(fruit);
+}
+
+fruits();
+console.log(fruit);
+
+const anotherFunction = () => {
+  var a = 1;
+  var a = 2;
+  let b = 1;
+  // let b = 2;
+  console.log(a);
+  console.log(b);
+}
+
+anotherFunction();
