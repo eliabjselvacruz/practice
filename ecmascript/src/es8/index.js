8fd043c8a13a9437456cb3e226121e6c025c9e6f
eliabjselvacruz <eliabjselvacruz51@gmail.com>
2022-03-03 16:41:36 -0600

    Practicing with async and await.

--- ecmascript/src/es8/index.js
+++ ecmascript/src/es8/index.js
@@ -22,3 +22,29 @@ console.log(values.length);
 const string ='hello';
 console.log(string.padStart(7,'hi'));
 console.log(string.padEnd(12,'*'));
+
+const helloWorl = () => {
+  return new Promise((resolve, reject) => {
+    true
+    ? setTimeout(() => {resolve('Hello')},3000)
+    : reject(new Error('Test Error'));
+  });
+};
+
+const helloAsync = async () => {
+  const hello = await helloWorl();
+  console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+  try {
+    const hello = await helloWorl();
+    console.log(hello);
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+anotherFunction();
