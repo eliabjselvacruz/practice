02ff5737d1f5fb83749ba59c89887d759dad2479
eliabjselvacruz <eliabjselvacruz51@gmail.com>
2022-03-03 14:49:01 -0600

    Practicing arrow functions, promises and object params.

--- ecmascript/src/es6/index.js
+++ ecmascript/src/es6/index.js
@@ -44,3 +44,37 @@ let team2 = ["Romel", "Camila", "Ana"];
 let education = [...team1, ...team2];
 
 console.log(education);
+
+
+let name = "Eliab";
+let age = 24;
+
+//ECMAScrip5
+let obj = {name: name, age: age};
+
+//ECMAScrip6
+let obj2 = {name, age};
+
+console.log(obj);
+console.log(obj2);
+
+const names = [{name:"Ana",age:22},{name:"Lila",age:26}];
+
+let list = names.map((item) => {
+  console.log(item.name);
+});
+
+const square = num => num*num;
+
+
+//Promise
+const helloPromise = () => {
+  return new Promise((resolve, reject) =>{
+    true ? resolve("Hi!") : reject("Ups!");
+  });
+}
+
+helloPromise()
+  .then((response)=>{console.log(response)})
+  .then(()=>{console.log("Other then")})
+  .catch((error)=>{console.log(error)});
