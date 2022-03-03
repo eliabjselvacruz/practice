ab282134dcf99f2998f2ba3c1f4203bdc40f48d2
eliabjselvacruz <eliabjselvacruz51@gmail.com>
2022-03-03 13:41:47 -0600

    Practicing multiline, spread operator and destructuring.

--- ecmascript/src/es6/index.js
+++ ecmascript/src/es6/index.js
@@ -21,3 +21,26 @@ console.log(epicPhrase);
 
 let epicPhrase2 = `${hello} ${world}!`;
 console.log(epicPhrase2);
+
+let lorem = `Empleando saltos de línea de
+otra forma con ECMAScript6.`;
+
+console.log(lorem);
+
+let person = {
+  "name": "Eiab",
+  "age": 24,
+  "country": "Ni"
+}
+
+console.log(person.name, person.age, person.country);
+
+let {name, age, country} = person;
+console.log(name, age, country);
+
+let team1 = ["María", "Pedro", "Juan"];
+let team2 = ["Romel", "Camila", "Ana"];
+
+let education = [...team1, ...team2];
+
+console.log(education);
